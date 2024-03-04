24a860db8c0114f200d1240ef6b1cb7db2cbdb50
06011-30-06 <06011-30-06@exchange.ad.afpanet>
2024-03-04 11:25:43 +0100

    V-0.0.1

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ArticlesController extends AbstractController
+{
+    #[Route('/articles', name: 'app_articles')]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+
+        $articles = $entityManager->getRepository(Article::class)->findAll();
+        // dd($articles);
+        return $this->render('articles/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+    #[Route('/articles/{id}', name: 'app_get_article_by_id')]
+    public function getArticleById(): Response
+    {
+        return $this->render('articles/show_artcile.html.twig', [
+            'controller_name' => 'ArticlesController',
+        ]);
+    }
+}
---
+++ src/Controller/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(): Response
+    {
+        return $this->render('contact/index.html.twig', [
+            'controller_name' => 'ContactController',
+            'muazu' => 'Nigeria is not the same thing of niger',
+        ]);
+    }
+}
