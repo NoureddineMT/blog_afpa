11560a8e233dd8b3fcf74141b7d10645b6c8e321
Noureddine Mouatamid <95962747+NoureddineMT@users.noreply.github.com>
2024-03-07 15:13:56 +0100

    V-0.0.3

--- migrations/Version20240307135029.php
+++ migrations/Version20240307135029.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240305134117 extends AbstractMigration
+final class Version20240307135029 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,12 @@ final class Version20240305134117 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE article ADD date DATE DEFAULT NULL');
-        $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E6612469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
-        $this->addSql('CREATE INDEX IDX_23A0E6612469DE2 ON article (category_id)');
+        $this->addSql('ALTER TABLE contact CHANGE name last_name VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE article DROP FOREIGN KEY FK_23A0E6612469DE2');
-        $this->addSql('DROP INDEX IDX_23A0E6612469DE2 ON article');
-        $this->addSql('ALTER TABLE article DROP date');
+        $this->addSql('ALTER TABLE contact CHANGE last_name name VARCHAR(255) NOT NULL');
     }
 }
