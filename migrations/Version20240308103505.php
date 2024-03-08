e2937a80ca22ed68367d5f5f8c876bc5eb13f722
06011-30-06 <06011-30-06@exchange.ad.afpanet>
2024-03-08 11:55:18 +0100

    V-0.0.5

--- migrations/Version20240308103505.php
+++ migrations/Version20240308103505.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240306100749 extends AbstractMigration
+final class Version20240308103505 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240306100749 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE article ADD date DATE DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE article DROP date');
+        $this->addSql('ALTER TABLE user DROP is_verified');
     }
 }
