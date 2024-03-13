194e180ba520a0d8ebc91ed0d0f7cd193766887c
06011-30-06 <06011-30-06@exchange.ad.afpanet>
2024-03-13 09:36:43 +0100

    V-0.0.6.2

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,30 +7,63 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('email', EmailType::class,[
-                'constraints' => [new Regex([
-                    'pattern' => '/@/',
-                    'message' => 'L\'adresse email doit contenir le caractère "@".'
-                ]),
-
-                ]
-            ])
-            ->add('adresse')
+        ->add('firstName', TextType::class, [
+            'label' => 'Prénom',
+            'attr' => [
+                'placeholder' => 'Votre prénom'
+            ],
+            'constraints' => [
+                new NotBlank(['message' => 'Votre prénom doit être renseigné']),
+                new Length(['min' => 2, 'minMessage' => 'Votre prénom doit faire minimum 2 caractères']),
+            ],
+            
+        ])
+        ->add('lastName', TextType::class, [
+            'label' => 'Nom',
+            'attr' => [
+                'placeholder' => 'Votre nom'
+            ],
+            'constraints' => [
+                new NotBlank(['message' => 'Votre nom doit être renseigné']),
+                new Length(['min' => 2, 'minMessage' => 'Votre nom doit faire minimum 2 caractères']),
+            ],
+            
+        ])
+        ->add('email', EmailType::class, [
+            'label' => 'Email',
+            'attr' => [
+                'placeholder' => 'Votre email'
+            ],
+            'constraints' => [
+                new NotBlank(['message' => 'Votre email doit être renseigné']),
+                new Email(['message' => 'Veuillez renseigner un email valide!']),
+            ],
+            
+        ])
+        ->add('adresse', TextType::class, [
+            'label' => 'Addresse',
+            'attr' => [
+                'placeholder' => 'Votre Addresse'
+            ],
+            'constraints' => [
+                new NotBlank(['message' => 'Votre adresse doit être renseignée']),
+            ],
+            
+        ])
             ->add('code_postale', TextType::class, [
                 'constraints' => [
                     new Length([
@@ -53,10 +86,10 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password', 'placeholder' => 'Votre code postal'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez renseigner un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
